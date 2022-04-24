ba8a6ea26d4994f468e8d12346efc847d922980d
carlinhoshk <carlosmdohk@gmail.com>
2022-04-24 19:54:40 -0300

    subindo aulas http

---
+++ src/com/httpexamples/getRequest.java
@@ -0,0 +1,40 @@
+package com.httpexamples;
+
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.sql.SQLOutput;
+import java.time.Duration;
+
+public class getRequest {
+
+    public static final String URL_GET = "https://jsonplaceholder.typicode.com/posts";
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        // cliente
+        HttpClient client = HttpClient.newHttpClient();
+
+        // requisição
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .timeout(Duration.ofSeconds(10))
+                .uri(URI.create(URL_GET))
+                .build();
+
+        // solicitação
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        // saida
+        System.out.println(response.statusCode());
+        System.out.println(response.body());
+
+
+
+
+
+    }
+}
---
+++ src/com/httpexamples/postRequest.java
@@ -0,0 +1,33 @@
+package com.httpexamples;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Path;
+import java.time.Duration;
+
+public class postRequest {
+
+    public static final String URL_POST = "http://httpbin.org/forms/post";
+    public static final String FILE_JSON = "C:\\Users\\carlo\\Desktop\\aula hackathon\\Introdução ao HTTPHTTPS\\HttpExample\\pedido.json";
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        // cliente
+        HttpClient client = HttpClient.newHttpClient();
+
+        // requisição
+        HttpRequest request = HttpRequest.newBuilder()
+                //.POST(HttpRequest.BodyPublishers.ofFile(Path.of(FILE_JSON)))
+                .timeout(Duration.ofSeconds(10))
+                .uri(URI.create(URL_POST))
+                .build();
+
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(System.out::println)
+                .join();
+    }
+}
